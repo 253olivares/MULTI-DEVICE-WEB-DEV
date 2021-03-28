73124dc469d43f6d2fc0616e0419c6cf27a6c503
Madmanz13579 <67510851+253olivares@users.noreply.github.com>
2021-03-28 02:57:51 -0400

    changes

--- homework-eight/app/app.js
+++ homework-eight/app/app.js
@@ -2,11 +2,11 @@ var userVarName = "Variable";
 
 //code for the home page
 var homeContent = `<div class="hero_image">
-<h1>Security Agency Name</h1>
+<h1>Cyber Security Agency Name</h1>
 </div>
 <div class="contentHome1">
 <img src="images/Number1.png" alt="">
-<h3>Voted Number 1 Security Agency in 2020</h3>
+<h3>Voted Number 1 Cyber Security Agency in 2020</h3>
 </div>
 <div class="hero_image2">
 <h1>Real Time Support</h1>
@@ -51,8 +51,23 @@ var homeContent = `<div class="hero_image">
 
 
 
+//cpmtemt for my about page
+var aboutContent = ` <div class="founder">
+<img src="images/Chad.jpg" alt="">
+<h3>Isaac Newton</h3>
+<br>
+<p>Founder of Cyber Security Agency after getting his information stolen several times Issac decided to
+    begin his own secure
+    agency that
+    centers in helping proctect consumers. Issac began his journy in 2012 to establish a trustworthy and
+    securty agency where you can rely on our protection to keep your data from being stolen!</p>
+</div>
+<div class="location">
+<h1>WHERE?</h1>
+</div>`;
 
-var aboutContent = "<h1>ABOUT</h1><p>This is a page</p>";
+
+
 var productsContent = "<h1>PRODUCTS</h1><p>This is a page</p>";
 var contactContent = "<h1>CONTACT</h1><p>This is a page</p>";
 
